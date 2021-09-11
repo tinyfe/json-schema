61cde6b07f9e8a74e3542e7d38c8b37e0eec893c
Rain120 <chao1202016@outlook.com>
2021-09-11 14:22:43 +0800

    feat: site next config, add monaco editor

--- site/next.config.js
+++ site/next.config.js
@@ -1,24 +1,20 @@
-const path = require('path');
 const withAntdLess = require('next-plugin-antd-less');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-/** @type {import('next').NextConfig} */
-module.exports = withBundleAnalyzer(
-  withAntdLess({
-    reactStrictMode: true,
-    distDir: 'build',
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      // Important: return the modified config
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@': path.resolve(__dirname, '.'),
-        styles: path.resolve(__dirname, 'styles'),
-        components: path.resolve(__dirname, 'components'),
-      };
+const config = {
+  reactStrictMode: true,
+  distDir: 'build',
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
 
-      return config;
-    },
-  }),
-);
+    return config;
+  },
+};
+
+/** @type {import('next').NextConfig} */
+module.exports = withBundleAnalyzer(withAntdLess(config));
